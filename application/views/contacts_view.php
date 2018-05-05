1bf4e4b02b566a2721a6ee60d902a47c8fb37df7
JeK2a <vipjonpc@mail.ru>
2018-05-05 16:32:38 +0300

    Добавлена библиотека bootstrap

--- application/views/contacts_view.php
+++ application/views/contacts_view.php
@@ -3,5 +3,5 @@
 Телефоны для связи:<br>
 +7(981)108-91-28 (Viber, Telegram)<br>
 <br/>
-<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ad788fa721f190f282342822ac6d9b0c97d50249af6998c7183b18c3389fa0220&amp;width=500&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+
 </p>
