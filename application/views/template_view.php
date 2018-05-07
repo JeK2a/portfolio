43dcdbce15c1b376a38fc02e556b8930c228c51d
JeK2a <31507511+JeK2a@users.noreply.github.com>
2018-05-07 18:46:51 +0300

    Перезалито

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -1,111 +1,70 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <meta name="description" content=""/>
-    <meta name="keywords" content=""/>
-
-
-<!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">-->
-
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css"/>
-    <link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" type="text/css" href="/css/style.css"/>
-
-    <title>JeK2a</title>
-
-<!--    <script src="/js/jquery-1.6.2.js" type="text/javascript"></script>-->
-
-</head>
-<body>
-<div id="wrapper">
-
-    <div id="header">
-        <div id="logo">
-            <p>
-                <a href=""><img src="../images/logo.jpg"  width="180" " alt="lorem"></a>
-            </p>
-            <!--					<a href="/">JeK2a</span> <span class="cms">Компани</span></a>-->
-        </div>
-        <div id="menu">
-            <ul>
-                <li class="first active"><a href="/">Главная</a></li>
-                <li><a href="/services">Услуги</a></li>
-                <li><a href="/portfolio">Портфолио</a></li>
-                <li><a href="/video">Видео</a></li>
-                <li class="last"><a href="/contacts">Контакты</a></li>
-            </ul>
-            <br class="clearfix"/>
-        </div>
-    </div>
-
-    <div id="page">
-        <div id="sidebar">
-            <div class="side-box">
-                <h3>Основное меню</h3>
-                <ul class="list">
-                    <li class="first "><a href="/">Главная</a></li>
-                    <li><a href="/services">Услуги</a></li>
-                    <li><a href="/portfolio">Примеры работ</a></li>
-                    <li><a href="/video">Видео</a></li>
-                    <li class="last"><a href="/contacts">Контакты</a></li>
-                </ul>
-            </div>
-        </div>
-        <div id="content">
-            <div class="box">
-                <?php include 'application/views/'.$content_view; ?>
-            </div>
-            <br class="clearfix"/>
-        </div>
-        <br class="clearfix"/>
-    </div>
-
-    <div id="page-bottom">
-        <div id="page-bottom-sidebar">
-            <h3>Наши контакты</h3>
-            <ul class="list">
-                <li class="first">Телефоны для связи:</li>
-                <li class="last">+7(978)263-0-264 (Viber, WhatsApp, Telegram)</li>
-            </ul>
-        </div>
-        <br class="clearfix"/>
-    </div>
-
-</div>
-<div id="footer">
-    <a href="/">JeK2a</a> &copy; 2018</a>
-</div>
-
-<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
-<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>-->
-<!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>-->
-
-</body>
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+		<meta name="description" content=""/>
+		<meta name="keywords" content=""/>
+		<title>JeK2a</title>
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css"/>
+		<link href="http://fonts.googleapis.com/css?family=Kreon" rel="stylesheet" type="text/css"/>
+		<link rel="stylesheet" type="text/css" href="/css/style.css"/>
+		<script src="/js/jquery-1.6.2.js" type="text/javascript"></script>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<div id="logo">
+                    <p>
+                        <a href=""><img src="../images/logo.jpg"  width="180" " alt="lorem"></a>
+                    </p>
+<!--					<a href="/">JeK2a</span> <span class="cms">Компани</span></a>-->
+				</div>
+				<div id="menu">
+					<ul>
+						<li class="first active"><a href="/">Главная</a></li>
+						<li><a href="/services">Услуги</a></li>
+						<li><a href="/portfolio">Портфолио</a></li>
+                        <li><a href="/video">Видео</a></li>
+						<li class="last"><a href="/contacts">Контакты</a></li>
+					</ul>
+					<br class="clearfix"/>
+				</div>
+			</div>
+			<div id="page">
+				<div id="sidebar">
+					<div class="side-box">
+						<h3>Основное меню</h3>
+						<ul class="list">
+							<li class="first "><a href="/">Главная</a></li>
+							<li><a href="/services">Услуги</a></li>
+							<li><a href="/portfolio">Примеры работ</a></li>
+                            <li><a href="/video">Видео</a></li>
+							<li class="last"><a href="/contacts">Контакты</a></li>
+						</ul>
+					</div>
+				</div>
+				<div id="content">
+					<div class="box">
+						<?php include 'application/views/'.$content_view; ?>
+					</div>
+					<br class="clearfix"/>
+				</div>
+				<br class="clearfix"/>
+			</div>
+			<div id="page-bottom">
+				<div id="page-bottom-sidebar">
+					<h3>Наши контакты</h3>
+					<ul class="list">
+                        <li class="first">Телефоны для связи:</li>
+                        <li class="last">+7(978)263-0-264 (Viber, WhatsApp, Telegram)</li>
+					</ul>
+				</div>
+				<br class="clearfix"/>
+			</div>
+		</div>
+		<div id="footer">
+			<a href="/">JeK2a</a> &copy; 2018</a>
+		</div>
+	</body>
 </html>
-
-
-
-<!--<!doctype html>-->
-<!--<html lang="en">-->
-<!--<head>-->
-<!--    <!-- Required meta tags -->-->
-<!--    <meta charset="utf-8">-->
-<!--    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">-->
-<!---->
-<!--    <!-- Bootstrap CSS -->-->
-<!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">-->
-<!---->
-<!--    <title>Hello, world!</title>-->
-<!--</head>-->
-<!--<body>-->
-<!---->
-<!---->
-<!--<!-- Optional JavaScript -->-->
-<!--<!-- jQuery first, then Popper.js, then Bootstrap JS -->-->
-<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
-<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>-->
-<!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>-->
-<!--</body>-->
-<!--</html>-->
